ffb1a82acdebacc7ac3d337f9477e46e1b00b5eb
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-02 02:59:41 +0600

    distinct and sorted with streams

--- section 03/Playing with streams/src/programming/FP02Exercise.java
+++ section 03/Playing with streams/src/programming/FP02Exercise.java
@@ -1,26 +1,50 @@
-    package programming;
+package programming;
 
-    import java.util.Arrays;
-    import java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
-    public class FP02Exercise {
-        public static void main(String[] args) {
-            List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-            List<String> courses = Arrays.asList("Java", "C", "C++", "Python", "JavaScript", "Go", "TypeScript", "Rust");
+public class FP02Exercise {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        List<String> courses = Arrays.asList("Java", "C", "C++", "Python", "JavaScript", "Go", "TypeScript", "Rust");
 
-            // exercise for reduce and streams
+        System.out.println("Sum of squares: " + sumOfSquares(numbers));
+        System.out.println("Sum of cubes: " + sumOfCubes(numbers));
+        System.out.println("Sum of odd numbers: " + sumOfOddNumbers(numbers));
 
-    //        Square every number in a list and find the sum of squares.
-            int result, result2, result3;
-            result = numbers.stream().map(x -> x * x).reduce(0, Integer::sum);
-            System.out.println(result);
+        System.out.println("Distinct numbers:");
+        printDistinctNumbers(numbers);
 
-    //        Cube every number in a list and find sum of cube
-            result2 = numbers.stream().map(x -> x * x * x).reduce(0, Integer::sum);
-            System.out.println(result2);
+        System.out.println("Sorted numbers:");
+        printAllSortedNumbers(numbers);
 
-            // find sum of odd numbers in a list.
-            result3 = numbers.stream().filter(number -> number % 2 == 1).reduce(0, Integer::sum);
-            System.out.println(result3);
-        }
+        System.out.println("sorted and distinct numbers:");
+        printDistinctAndSortedNumbers(numbers);
     }
+
+    private static void printDistinctAndSortedNumbers(List<Integer> numbers) {
+        numbers.stream().distinct().forEach(System.out::println);
+    }
+
+    private static void printAllSortedNumbers(List<Integer> numbers) {
+        numbers.stream().sorted().forEach(System.out::println);
+    }
+
+    private static int sumOfSquares(List<Integer> numbers) {
+        return numbers.stream().map(x -> x * x).reduce(0, Integer::sum);
+    }
+
+    private static int sumOfCubes(List<Integer> numbers) {
+        return numbers.stream().map(x -> x * x * x).reduce(0, Integer::sum);
+    }
+
+    private static int sumOfOddNumbers(List<Integer> numbers) {
+        return numbers.stream().filter(number -> number % 2 == 1).reduce(0, Integer::sum);
+    }
+
+    private static void printDistinctNumbers(List<Integer> numbers) {
+        numbers.stream().distinct().forEach(System.out::println);
+    }
+
+
+}
