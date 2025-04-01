0409c8a44c51a9bf9ef7663a3d2502959a6d1295
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-01 20:43:13 +0600

    using map in functional programming

--- section 02/Functional Programming/src/programming/FP01Exercises.java
+++ section 02/Functional Programming/src/programming/FP01Exercises.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class FP01Exercises {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37);
+        List<Integer> numbers = Arrays.asList(8, 2, 3, 5, 7, 10, 13, 17, 20, 23, 29, 31, 32);
 //        printOddNumberFromTheList(numbers);
 
         List<String> courses = Arrays.asList("Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernates");
@@ -13,7 +13,34 @@ public class FP01Exercises {
 
 //        printCoursesThatContainsSpring(courses);
         
-        printCoursesWhoseNameHasAtleast4Letters(courses);
+//        printCoursesWhoseNameHasAtleast4Letters(courses);
+
+        // using mapping in functional programming
+//        printSquaresOfEvenNumber(numbers);
+
+//        printCubeOfOddNumber(numbers);
+        printTheNumberOfCharacterInEachCourse(courses);
+    }
+
+    private static void printTheNumberOfCharacterInEachCourse(List<String> courses) {
+        courses.stream()
+                .map(course -> course + " " + course.length())
+                .forEach(System.out::println);
+    }
+
+    private static void printCubeOfOddNumber(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number % 2 == 1)
+                .map(number -> number * number * number)
+                .forEach(System.out::println);
+    }
+
+    private static void printSquaresOfEvenNumber(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number % 2 == 0)
+                // mapping -x -> x * x
+                .map(number -> number * number)
+                .forEach(System.out::println);
     }
 
     private static void printCoursesWhoseNameHasAtleast4Letters(List<String> courses) {
@@ -38,6 +65,8 @@ public class FP01Exercises {
                 .filter(number -> number % 2 != 0)
                 .forEach(System.out::println);
     }
+
+
 }
 
 
