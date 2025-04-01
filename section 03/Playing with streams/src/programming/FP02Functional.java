1d6a765ddc043359e090a48bf4981f700434c1b4
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-02 01:31:28 +0600

    playing with streams and reduce

--- section 03/Playing with streams/src/programming/FP02Functional.java
+++ section 03/Playing with streams/src/programming/FP02Functional.java
@@ -6,8 +6,22 @@ public class FP02Functional {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 
-        int sum = addListFunctional(numbers);
-        System.out.println(sum);
+//        int sum = addListFunctional(numbers);
+//        System.out.println(sum);
+
+        playwithReduceOperation(numbers);
+    }
+
+    private static void playwithReduceOperation(List<Integer> numbers) {
+        int result;
+//        result = numbers.stream().reduce(0, Integer::sum);
+//        result = numbers.stream().reduce(0, (x, y) -> x + y);
+//        result = numbers.stream().reduce(0, (x, y) -> x);
+//        result = numbers.stream().reduce(0, (x, y) -> y);
+//        result = numbers.stream().reduce(0, (x, y) -> x > y ? x : y);
+//        result = numbers.stream().reduce(Integer.MIN_VALUE, (x, y) -> x > y ? x : y);
+        result = numbers.stream().reduce(Integer.MAX_VALUE, (x, y) -> x > y ? y : x);
+        System.out.println(result);
     }
 
 //    private static int sum(int a, int b) {
@@ -27,4 +41,6 @@ public class FP02Functional {
     }
 
     /* reduce operation takes list of numbers and reduce to one number */
+
+
 }
