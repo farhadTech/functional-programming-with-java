e3ce29dbc8f1a51a8e365981a761fed8c4fdd581
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-02 00:19:13 +0600

    playing with reduce stream operation

--- section 03/Playing with streams/src/programming/FP02Functional.java
+++ section 03/Playing with streams/src/programming/FP02Functional.java
@@ -10,15 +10,20 @@ public class FP02Functional {
         System.out.println(sum);
     }
 
-    private static int sum(int a, int b) {
-        return a + b;
-    }
+//    private static int sum(int a, int b) {
+//        return a + b;
+//    }
 
     private static int addListFunctional(List<Integer> numbers) {
         // combine them into one result -> one value
         // 0 and (a, b) -> a + b FP02Functional::sum
         return numbers.stream()
-                .reduce(0, FP02Functional::sum);
+//                .reduce(0, FP02Functional::sum);
+                // using lambda expression
+//                .reduce(0, (a, b) -> a + b);
+                // more simplify
+                .reduce(0, Integer::sum);
+
     }
 
     /* reduce operation takes list of numbers and reduce to one number */
