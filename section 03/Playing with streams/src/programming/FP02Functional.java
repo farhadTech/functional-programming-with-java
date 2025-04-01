ee174724e5be4ce9e5373ec4d9d27b069247a369
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-02 00:03:38 +0600

    calculate sum using reduce

---
+++ section 03/Playing with streams/src/programming/FP02Functional.java
@@ -0,0 +1,25 @@
+package programming;
+
+import java.util.List;
+
+public class FP02Functional {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+        int sum = addListFunctional(numbers);
+        System.out.println(sum);
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+
+    private static int addListFunctional(List<Integer> numbers) {
+        // combine them into one result -> one value
+        // 0 and (a, b) -> a + b FP02Functional::sum
+        return numbers.stream()
+                .reduce(0, FP02Functional::sum);
+    }
+
+    /* reduce operation takes list of numbers and reduce to one number */
+}
