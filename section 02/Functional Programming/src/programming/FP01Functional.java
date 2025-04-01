772f0dc4ac6702852ff0597711257bb432c5fec8
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-01 14:04:54 +0600

    improving java functional approach with filter

--- section 02/Functional Programming/src/programming/FP01Functional.java
+++ section 02/Functional Programming/src/programming/FP01Functional.java
@@ -4,19 +4,39 @@ import java.util.List;
 
 public class FP01Functional {
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 4, 12, 15));
-    }
-
-    private static void print(int number) {
-        System.out.print(number + " ");
+        List<Integer>numbers = List.of(12, 9, 13, 4, 6, 4, 12, 15);
+//        printAllNumbersInListFunctional(numbers);
+//        printEvenNumbersInListTraditional(numbers);
+        printEvenNumbersInListFunctional(numbers);
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
         numbers.stream()
-                .forEach(FP01Functional::print);
+                .forEach(System.out::println);
+    }
 
+    private static void printEvenNumbersInListTraditional(List<Integer> numbers) {
+        // using traditional approach
+        for(Integer number : numbers) {
+            if(number % 2 == 0) {
+                System.out.println(number + " is even");
+            } else {
+                System.out.println(number + " is odd");
+            }
+        }
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(FP01Functional::isEven)
+                .forEach(System.out::println);
     }
 }
+
 
 
 
