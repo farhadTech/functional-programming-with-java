66307f74ac455582470be509438919ce19742c1a
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-03 02:24:30 +0600

    allMatch, noneMatch, anyMatch

--- section 05/Functional Programming with Custom Classes/src/programming/CustomClass.java
+++ section 05/Functional Programming with Custom Classes/src/programming/CustomClass.java
@@ -1,70 +1,41 @@
 package programming;
 
+import programming.Course;
+import programming.CustomClass;
+
 import java.util.List;
-
-class Course {
-    private String name;
-    private String category;
-    private int reviewScore;
-    private int noOfStudents;
-
-    public Course(String name, String category, int reviewScore, int noOfStudents) {
-        this.name = name;
-        this.category = category;
-        this.reviewScore = reviewScore;
-        this.noOfStudents = noOfStudents;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public int getReviewScore() {
-        return reviewScore;
-    }
-
-    public void setReviewScore(int reviewScore) {
-        this.reviewScore = reviewScore;
-    }
-
-    public int getNoOfStudents() {
-        return noOfStudents;
-    }
-
-    public void setNoOfStudents(int noOfStudents) {
-        this.noOfStudents = noOfStudents;
-    }
-
-    public String toString() {
-        return name + " " + category + " " + reviewScore + " " + noOfStudents;
-    }
-}
+import java.util.function.Predicate;
 
 public class CustomClass {
-    List<Course> courses =
-            List.of(
-                    new Course("Spring", "Framework", 98, 20000),
-                    new Course("Spring Boot", "Framework", 95, 18000),
-                    new Course("API", "Microservices", 97, 22000),
-                    new Course("Microservices", "Microservices", 96, 25000),
-                    new Course("FullStack", "FullStack", 91, 14000),
-                    new Course("AWS", "Cloud", 92, 21000),
-                    new Course("Azure", "Cloud", 99, 21000),
-                    new Course("Docker", "Cloud", 92, 20000),
-                    new Course("Kubernetes", "Cloud", 91, 20000)
-            );
+    public static void main(String[] args) {
+        List<Course> courses =
+                List.of(
+                        new Course("Spring", "Framework", 98, 20000),
+                        new Course("Spring Boot", "Framework", 95, 18000),
+                        new Course("API", "Microservices", 97, 22000),
+                        new Course("Microservices", "Microservices", 96, 25000),
+                        new Course("FullStack", "FullStack", 91, 14000),
+                        new Course("AWS", "Cloud", 92, 21000),
+                        new Course("Azure", "Cloud", 99, 21000),
+                        new Course("Docker", "Cloud", 92, 20000),
+                        new Course("Kubernetes", "Cloud", 91, 20000)
+                );
+
+        // allMatch, noneMatch, anyMatch
+        System.out.println(courses.stream().allMatch(course -> course.getReviewScore() > 90)); // true
+
+        Predicate<Course> reviewScoreGreaterThan95Predicate = course -> course.getReviewScore() > 95;
+        Predicate<Course> reviewScoreGreaterThan90Predicate = course -> course.getReviewScore() > 90;
+        Predicate<Course> reviewScoreLessThan90Predicate = course -> course.getReviewScore() < 90;
+
+        System.out.println(courses.stream().noneMatch(reviewScoreGreaterThan95Predicate)); // false
+        System.out.println(courses.stream().noneMatch(reviewScoreGreaterThan90Predicate)); // false
+        System.out.println(courses.stream().noneMatch(reviewScoreLessThan90Predicate)); // true
+        System.out.println(courses.stream().anyMatch(reviewScoreLessThan90Predicate)); // false
+        System.out.println(courses.stream().allMatch(reviewScoreLessThan90Predicate)); // false
+        System.out.println(courses.stream().allMatch(reviewScoreGreaterThan90Predicate)); // true
+
+    }
 }
 
 
