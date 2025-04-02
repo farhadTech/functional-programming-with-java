0f130c8f523f1756d9d3694986dcb6569e4c61b9
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-02 11:33:58 +0600

    Functional Interfaces Predicate, Function and Consumer

--- section 04/Exploring Java Functional Interfaces and Lambdas/src/programming/FunctionalInterfaces.java
+++ section 04/Exploring Java Functional Interfaces and Lambdas/src/programming/FunctionalInterfaces.java
@@ -1,6 +1,9 @@
 package programming;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FunctionalInterfaces {
     /*
@@ -15,12 +18,42 @@ public class FunctionalInterfaces {
      */
 
     public static void main(String[] args) {
-    List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 
-    numbers.stream()
-            .filter(n -> n % 2 == 0)
-            .map(n -> n * n)
-//            .forEach(x -> System.out.println(x));
-            .forEach(System.out::println);
+        Predicate<Integer> isEvenPredicate = number -> number % 2 == 0;
+
+        Predicate<Integer> isEvenPredicate2 = new Predicate<Integer>() {
+
+            @Override
+            public boolean test(Integer x) {
+                return x % 2 == 0;
+            }
+        };
+
+        Function<Integer, Integer> squareFunction = x -> x * x;
+
+        Function<Integer, Integer> squareFunction2 = new Function<Integer, Integer>() {
+
+            @Override
+            public Integer apply(Integer x) {
+                return x * x;
+            }
+        };
+
+        Consumer<Integer> sysoutConsumer = System.out::println;
+        Consumer<Integer> sysoutConsumer2 = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer x) {
+                System.out.println(x);
+            }
+        };
+
+        numbers.stream()
+                .filter(isEvenPredicate2)
+                .map(squareFunction2)
+                .forEach(sysoutConsumer2);
+
+
+        /* Behind every lambda expression is we write there is functional interfaces. */
     }
 }
