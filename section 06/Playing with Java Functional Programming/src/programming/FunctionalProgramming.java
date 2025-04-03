3f7aa7a70264c0e35642959a25b177b5b961aa9c
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-03 23:48:48 +0600

    Creating Streams for First 100 Numbers squares of Numbers and More.

--- section 06/Playing with Java Functional Programming/src/programming/FunctionalProgramming.java
+++ section 06/Playing with Java Functional Programming/src/programming/FunctionalProgramming.java
@@ -1,6 +1,8 @@
 package programming;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class FunctionalProgramming {
@@ -27,6 +29,29 @@ public class FunctionalProgramming {
         System.out.println(Arrays.stream(numberArray).average());
         System.out.println(Arrays.stream(numberArray).min());
         System.out.println(Arrays.stream(numberArray).max());
+
+
+        /** Creating Streams for First 100 Numbers, squares of Numbers and More:
+         * Sometime we would create a streams of primitive value much more dynamically.
+         */
+
+        IntStream.range(1, 10)
+                .forEach(x -> System.out.print(x + " "));
+        System.out.println();
+
+        // exclude 10
+        System.out.println(IntStream.range(1, 10).sum());
+
+        // include 10
+        System.out.println(IntStream.rangeClosed(1, 10).sum());
+
+
+        System.out.println(IntStream.iterate(1, e -> e + 2).limit(10).peek(x -> System.out.print(x + " ")).sum());
+        System.out.println(IntStream.iterate(2, e -> e + 2).limit(10).peek(x -> System.out.print(x + " ")).sum());
+        System.out.println(IntStream.iterate(2, e -> e * 2).limit(10).peek(x -> System.out.print(x + " ")).sum());
+        System.out.println(IntStream.iterate(2, e -> e * 2).limit(10).boxed().collect(Collectors.toList()));
+
+
     }
 }
 
